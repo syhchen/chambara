dcfcb71d0444c78c5cc3819a391d6932d3409be4
Stephen Chen <hello@stephenc.me>
2015-10-20 04:04:58 -0700

    Adds scroll to downloads.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 // var scene = document.getElementById('scene');
 // var parallax = new Parallax(scene);
 
-// TODO: add scroll on "get the demo" click
+
+$('#download-link').click(function(e) {
+    e.preventDefault();
+    $('html,body').animate({
+       scrollTop: $("#download").offset().top
+    });
+});
